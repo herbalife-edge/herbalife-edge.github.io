ede810081c732eeb193130b552cae3138aae60c5
herbalife-edge <herbalife.leads.edge@gmail.com>
2016-11-17 10:53:26 +0530

    updated the feature content, added home links to all pages

--- js/grayscale.js
+++ js/grayscale.js
@@ -32,13 +32,14 @@ $('.navbar-collapse ul li a').click(function() {
     $(this).closest('.collapse').collapse('toggle');
 });
 
+/*
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
     map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
-});
+});*/
 
 function init() {
     // Basic options for a simple Google Map
@@ -48,7 +49,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        //center: new google.maps.LatLng(40.6700, -73.9400), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -57,7 +58,7 @@ function init() {
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
+        /*styles: [{
             "featureType": "water",
             "elementType": "geometry",
             "stylers": [{
@@ -165,21 +166,21 @@ function init() {
                 "weight": 1.2
             }]
         }]
-    };
+    };*/
 
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+    //var mapElement = document.getElementById('map');
 
     // Create the Google Map using out element and options defined above
-    map = new google.maps.Map(mapElement, mapOptions);
+    //map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
+    /*var image = 'img/map-marker.png';
     var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
         icon: image
-    });
+    });*/
 }
